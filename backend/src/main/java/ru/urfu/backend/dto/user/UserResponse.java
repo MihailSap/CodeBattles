5bf086b8ff14d653b72504bed6ac6be4d1ecd753
Sapegin Mihail <sapegin2005@yandex.ru>
2026-02-22 22:33:32 +0500

    Добавил Dockerfile и Docker Compose, исправил версию

--- backend/src/main/java/ru/urfu/backend/dto/user/UserResponse.java
+++ backend/src/main/java/ru/urfu/backend/dto/user/UserResponse.java
@@ -1,4 +1,7 @@
 package ru.urfu.backend.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Пользователь")
 public record UserResponse(String email, String login) {
 }
